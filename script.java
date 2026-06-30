e1b1fc1b8625845cbebdfdad4532377f293e974c
Draszcz Jordan <jordan@imac-de-draszcz.home>
2026-06-30 16:29:25 +0200

    Just for commiting

---
+++ script.java
@@ -0,0 +1,16 @@
+public class script {
+
+    int a;
+    int b;
+
+    public static void main (String[] args){
+        System.out.println("Hello World");
+        System.out.println(test(343,23));
+
+
+    }
+
+    public static int test(int a, int b){
+        return a + b;
+    }
+}
