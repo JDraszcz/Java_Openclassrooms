6dc75cb610c4e5b108071cb39bcc2694e9cb8fc3
Draszcz Jordan <jordan@imac-de-draszcz.home>
2026-07-01 17:33:56 +0200

    Addin an array to study, needs to do an array list to try differences between those arrays

--- script.java
+++ script.java
@@ -1,4 +1,7 @@
+
 public class script {
+
+    static String[] names = new String[4];
 
     int a;
     int b;
@@ -7,10 +10,32 @@ public class script {
         System.out.println("Hello World");
         System.out.println(test(343,23));
 
+        namesList();
+        // Now executing the command to fill the array, add parameters if needed
+        System.out.println("Members of the family are :");
+        for(int i = 0; i < 4; i++){
+            System.out.println(names[i]);
+        }
 
     }
 
     public static int test(int a, int b){
         return a + b;
     }
+
+    public static void namesList(){
+
+    String name1 = "Jordan";
+    String name2 = "Margot";
+    String name3 = "Gabriel";
+    String name4 = "Rose";
+
+    names[0] = name1;
+    names[1] = name2;
+    names[2] = name3;
+    names[3] = name4;
+
+    }
+
+
 }
